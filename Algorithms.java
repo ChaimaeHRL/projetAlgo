1503d8e888784560943777dd4ac892ac16f4563a
ChaimaeHRL <elharoualchaimae@gmail.com>
2024-04-30 21:56:59 +0200

    s

--- Algorithms.java
+++ Algorithms.java
@@ -4,19 +4,100 @@ public interface Algorithms {
     Random rng = new Random();
 
     static void main(String[] args) {
-        // Your code here
-        System.out.println("Hello world!");
+        int[][] monstersToFill = new int[11][7];
+        int[][] treasuresToFill = new int[11][7];
+        GT.generateMonstersAndTreasures(monstersToFill, treasuresToFill);
+        GT.check(monstersToFill, treasuresToFill); // Appel de la méthode de vérification
+        showArray("Monsters:", monstersToFill);
+        showArray("Treasures:", treasuresToFill);
+
+        // Fusionner les tables de monstres et de trésors dans un seul board
+        int[][] mergedBoard = BoardMerger.merge(monstersToFill, treasuresToFill);
+        showArray("Merged Board:", mergedBoard);
     }
 
     /* --- Generate & Test --- */
     interface GT {
         static void generateMonstersAndTreasures(int[][] monstersToFill, int[][] treasuresToFill) {
-            //TODO
+            for (int i = 0; i < monstersToFill.length; i++) {
+                for (int j = 0; j < monstersToFill[i].length; j++) {
+                    int randomValue = rng.nextInt(6); // Génère un nombre entre 0 et 5
+                    if (randomValue == 0 || randomValue == 1) { // 1 chance sur 3 d'être un monstre
+                        monstersToFill[i][j] = rng.nextInt(50) + 1; // +1 pour pousser la plage de nombre vers le haut
+                    } else if (randomValue == 2) { // 1 chance sur 6 d'être un trésor
+                        treasuresToFill[i][j] = rng.nextInt(99) + 1; // Valeur du trésor entre 1 et 99
+                    }
+                }
+            }
         }
 
-        /* --- Utility functions for GT --- */
-        //TODO (if you have any)
+        static void check(int[][] monsters, int[][] treasures) {
+            for (int i = 0; i < monsters.length; i++) {
+                int monstersCount = 0;
+                int treasuresCount = 0;
+                int totalMonstersValue = 0;
+                int totalTreasuresValue = 0;
+                for (int j = 0; j < monsters[i].length; j++) {
+                    if (monsters[i][j] != 0) {
+                        monstersCount++;
+                        totalMonstersValue += monsters[i][j];
+                    }
+                    if (treasures[i][j] != 0) {
+                        treasuresCount++;
+                        totalTreasuresValue += treasures[i][j];
+                    }
+                }
+                if (monstersCount < 2) {
+                    generateMonstersAndTreasures(monsters, treasures);
+                    monstersCount = 0;
+                }
+                if (treasuresCount > 2 || totalTreasuresValue > totalMonstersValue) {
+                    generateMonstersAndTreasures(monsters, treasures);
+                    treasuresCount = 0;
+                }
+            }
+        }
     }
+
+    /* --- Utility functions for GT --- */
+    static void showArray(String label, int[][] array) {
+        System.out.println(label);
+        for (int[] row : array) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println(); // Nouvelle ligne après chaque rangée
+        }
+    }
+
+    /* --- Board Merger --- */
+    static class BoardMerger {
+        static int[][] merge(int[][] monsters, int[][] treasures) {
+            int[][] mergedBoard = new int[monsters.length][monsters[0].length];
+            for (int i = 0; i < monsters.length; i++) {
+                for (int j = 0; j < monsters[i].length; j++) {
+                    if (monsters[i][j] != 0 && treasures[i][j] != 0) {
+                        // Si les deux tableaux ont des valeurs non nulles, choisir une aléatoirement
+                        if (rng.nextBoolean()) {
+                            mergedBoard[i][j] = monsters[i][j];
+                        } else {
+                            mergedBoard[i][j] = treasures[i][j];
+                        }
+                    } else if (monsters[i][j] != 0) {
+                        mergedBoard[i][j] = monsters[i][j];
+                    } else if (treasures[i][j] != 0) {
+                        mergedBoard[i][j] = treasures[i][j];
+                    } else {
+                        // Si les deux tableaux ont des valeurs nulles, laisser la case vide
+                        mergedBoard[i][j] = 0;
+                    }
+                }
+            }
+            return mergedBoard;
+        }
+    }
+}
+
 
     /* --- Divide & Conquer --- */
     interface DC {
@@ -52,4 +133,4 @@ public interface Algorithms {
 
     /* --- Common utility functions --- */
     //TODO (if you have any)
-}
+
