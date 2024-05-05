483fde681e7a14717e9d2702e4e1c3492e9aa36e
nmouhat <nouhaila.mouhat@student.unamur.be>
2024-05-05 18:26:59 +0200

    n

--- Algorithms.java
+++ Algorithms.java
@@ -1,65 +1,80 @@
-// méthode naive simple 
-//public int pathsCount ( int i , int j ) {
-//if ( i==0 || j==0 ) {
-//return 1 ;
-//} else {
-//return pathsCount ( i−1 , j ) + pathsCount ( i , j−1 ) + pathsCount (i−1,j-1);}
+interface DP {
+    static String perfectSolution(State state) {
+        int rows = state.monsters.length;
+        int cols = state.monsters[0].length;
+        ScorePath[][][] dp = new ScorePath[rows][cols][3]; // Troisième dimension pour les directions: 0 = gauche, 1 = droite, 2 = bas
 
-//programation_dynamique
-public int chemincount(int i, int j, int gridSize) {
-    int[][] chemin = new int[gridSize][gridSize];
-    // Move null check after initialization
-    if (chemin != null) {
-        return j;
-    }
-    for (int x = 0; x < chemin.length; x++) {
-        chemin[x][0] = 1;
-        chemin[0][x] = 1;
-    }
-    for (int x = 1; x < chemin.length; x++) {
-        for (int y = 1; y < chemin[x].length; y++) {
-            chemin[x][y] = chemin[x - 1][y] + chemin[x][y - 1] + chemin[x][y];
+        // Initialisation des chemins possibles avec un score minimal et un chemin vide
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                dp[i][j] = new ScorePath[] {new ScorePath(Integer.MIN_VALUE, ""), new ScorePath(Integer.MIN_VALUE, ""), new ScorePath(Integer.MIN_VALUE, "")};
+            }
         }
-    }
-    return chemin[i][j];
-}
-public int getSquareValue(int x, int y) {
-    // Supposons que grid soit votre grille représentée par une matrice d'entiers
-    // Assurez-vous que x et y sont des indices valides pour votre grille
-    return grid[x][y];
-}
 
-private void memoizeBestPaths(int gridSize) {
-    int[][] score;
-    Direction[][] directions;
-    score = new int[gridSize][gridSize];
-    directions = new String[gridSize][gridSize];
-    score[0][0] = getSquareValue(0, 0);
-    for (int i = 1; i < gridSize; i++) {
-        score[i][0] = score[i - 1][0] + getSquareValue(i, 0);
-        directions[i][0] = Direction.RIGHT; // Change Direction.Right to Direction.RIGHT
+        // Initialisation de la première ligne où le héros peut choisir de partir de n'importe quelle colonne
+        for (int j = 0; j < cols; j++) {
+            int initialScore = state.treasures[0][j] - state.monsters[0][j];
+            if (state.heroHealth + initialScore > 0) {
+                dp[0][j][2] = new ScorePath(initialScore, ""); // Commence ici sans direction horizontale
+            }
+        }
+
+        // Remplir le tableau dp
+        for (int i = 1; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (state.heroHealth > 0) { // Si le héros a encore de la santé
+                    updateDP(dp, state, i, j);
+                }
+            }
+        }
+
+        // Trouver le meilleur chemin a partir de la dernière ligne
+        ScorePath bestPath = new ScorePath(Integer.MIN_VALUE, "");
+        for (int j = 0; j < cols; j++) {
+            for (int d = 0; d < 3; d++) {
+                if (dp[rows - 1][j][d].score > bestPath.score) {
+                    bestPath = dp[rows - 1][j][d];
+                }
+            }
+        }
+        return bestPath.path;
     }
-    for (int j = 1; j < gridSize; j++) {
-        score[0][j] = score[0][j - 1] + getSquareValue(0, j);
-        directions[0][j] = Direction.UP; // Change Direction.Up to Direction.UP
-    }
-    for (int i = 1; i < gridSize; i++) {
-        for (int j = 1; j < gridSize; j++) {
-            int left = score[i - 1][j] + getSquareValue(i, j);
-            int bottom = score[i][j - 1] + getSquareValue(i, j);
-            int diag = score[i - 1][j - 1] + getSquareValue(i, j); // Fix '-' character
-            if (left >= bottom && left >= diag) {
-                score[i][j] = left;
-                directions[i][j] = Direction.RIGHT;
-            } else if (bottom >= left && bottom >= diag) {
-                score[i][j] = bottom;
-                directions[i][j] = Direction.UP;
-            } else {
-                score[i][j] = diag;
-                directions[i][j] = Direction.DIAGONALLY;
+
+    static void updateDP(ScorePath[][][] dp, State state, int i, int j) {
+        int rows = state.monsters.length;
+        int cols = state.monsters[0].length;
+
+        for (int dir = 0; dir < 3; dir++) {
+            if (dp[i-1][j][dir].score != Integer.MIN_VALUE) { // Héritage du score de l'étage précédent
+                int score = dp[i-1][j][dir].score + state.treasures[i][j] - state.monsters[i][j];
+                if (score > 0) { // Continuation vers le bas si possible
+                    dp[i][j][2] = new ScorePath(score, dp[i-1][j][dir].path + "d");
+                }
+            }
+        }
+
+        // Continuation vers la gauche ou la droite
+        if (j > 0 && dp[i][j-1][0].score != Integer.MIN_VALUE) { // Gauche
+            int leftScore = dp[i][j-1][0].score + state.treasures[i][j] - state.monsters[i][j];
+            if (leftScore > 0) {
+                dp[i][j][0] = new ScorePath(leftScore, dp[i][j-1][0].path + "l");
+            }
+        }
+        if (j < cols - 1 && dp[i][j+1][1].score != Integer.MIN_VALUE) { // Droite
+            int rightScore = dp[i][j+1][1].score + state.treasures[i][j] - state.monsters[i][j];
+            if (rightScore > 0) {
+                dp[i][j][1] = new ScorePath(rightScore, dp[i][j+1][1].path + "r");
             }
         }
     }
+
+    class ScorePath {
+        int score;
+        String path;
+
+        ScorePath(int score, String path) {
+            this.score = score;
+            this.path = path;
+        }
+    }
 }
-
-
