b27b55e3b8ef68c4c49a77d5b6b27bc0203d114e
Rymabelkhiri <ryma.belkhiri@student.unamur.be>
2024-05-10 16:29:49 +0200

    r

--- LevelGenerator.java
+++ LevelGenerator.java
@@ -1,82 +1,79 @@
-import java.util.Arrays;
-
 /* --- Divide & Conquer --- */
 interface DC {
     static void sortLevel(int[][] monstersToSort, int[][] treasuresToSort) {
-        int numRows = monstersToSort.length;
-        int[] rowValues = new int[numRows];
-        for (int i = 0; i < numRows; i++) {
-            int treasureSum = Arrays.stream(treasuresToSort[i]).sum();
-            int monsterSum = Arrays.stream(monstersToSort[i]).map(Math::abs).sum();
-            rowValues[i] = treasureSum - monsterSum;
+        int[] rowValues = new int[monstersToSort.length];
+        for (int i = 0; i < monstersToSort.length; i++) {
+            rowValues[i] = calculateRowValue(monstersToSort[i], treasuresToSort[i]);
         }
-
-        triFusion(monstersToSort, treasuresToSort, rowValues, 0, numRows - 1);
+        mergeSort(monstersToSort, treasuresToSort, rowValues, 0, monstersToSort.length - 1);
     }
 
-    static void triFusion(int[][] monsters, int[][] treasures, int[] rowValues, int debut, int fin) {
-        if (debut < fin) {
-            int milieu = (debut + fin) / 2;
-            triFusion(monsters, treasures, rowValues, debut, milieu);
-            triFusion(monsters, treasures, rowValues, milieu + 1, fin);
-            fusionner(monsters, treasures, rowValues, debut, milieu, fin);
+    static int calculateRowValue(int[] monsters, int[] treasures) {
+        int totalTreasures = Arrays.stream(treasures).sum();
+        int totalMonsterStrengths = Arrays.stream(monsters).sum();
+        return totalTreasures - totalMonsterStrengths;
+    }
+
+    static void mergeSort(int[][] monsters, int[][] treasures, int[] rowValues, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(monsters, treasures, rowValues, low, mid);
+            mergeSort(monsters, treasures, rowValues, mid + 1, high);
+            merge(monsters, treasures, rowValues, low, mid, high);
         }
     }
 
-    static void fusionner(int[][] monsters, int[][] treasures, int[] rowValues, int debut, int milieu, int fin) {
-        int n1 = milieu - debut + 1;
-        int n2 = fin - milieu;
+    static void merge(int[][] monsters, int[][] treasures, int[] rowValues, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
 
-        int[][] gaucheMonsters = new int[n1][];
-        int[][] gaucheTreasures = new int[n1][];
-        int[] gaucheValues = new int[n1];
-        int[][] droiteMonsters = new int[n2][];
-        int[][] droiteTreasures = new int[n2][];
-        int[] droiteValues = new int[n2];
+        int[][] leftMonsters = new int[n1][];
+        int[][] leftTreasures = new int[n1][];
+        int[] leftRowValues = new int[n1];
+
+        int[][] rightMonsters = new int[n2][];
+        int[][] rightTreasures = new int[n2][];
+        int[] rightRowValues = new int[n2];
 
         for (int i = 0; i < n1; ++i) {
-            gaucheMonsters[i] = monsters[debut + i];
-            gaucheTreasures[i] = treasures[debut + i];
-            gaucheValues[i] = rowValues[debut + i];
+            leftMonsters[i] = monsters[low + i];
+            leftTreasures[i] = treasures[low + i];
+            leftRowValues[i] = rowValues[low + i];
         }
         for (int j = 0; j < n2; ++j) {
-            droiteMonsters[j] = monsters[milieu + 1 + j];
-            droiteTreasures[j] = treasures[milieu + 1 + j];
-            droiteValues[j] = rowValues[milieu + 1 + j];
+            rightMonsters[j] = monsters[mid + 1 + j];
+            rightTreasures[j] = treasures[mid + 1 + j];
+            rightRowValues[j] = rowValues[mid + 1 + j];
         }
 
-        int i = 0, j = 0;
-        int k = debut;
+        int i = 0, j = 0, k = low;
         while (i < n1 && j < n2) {
-            // Placer les rangées avec plus de monstres en haut
-            if (gaucheValues[i] <= droiteValues[j]) {
-                monsters[k] = gaucheMonsters[i];
-                treasures[k] = gaucheTreasures[i];
-                rowValues[k] = gaucheValues[i];
+            if (leftRowValues[i] <= rightRowValues[j]) {
+                monsters[k] = leftMonsters[i];
+                treasures[k] = leftTreasures[i];
+                rowValues[k] = leftRowValues[i];
                 i++;
             } else {
-                monsters[k] = droiteMonsters[j];
-                treasures[k] = droiteTreasures[j];
-                rowValues[k] = droiteValues[j];
+                monsters[k] = rightMonsters[j];
+                treasures[k] = rightTreasures[j];
+                rowValues[k] = rightRowValues[j];
                 j++;
             }
             k++;
         }
 
-        // Ajouter les rangées restantes de gauche
         while (i < n1) {
-            monsters[k] = gaucheMonsters[i];
-            treasures[k] = gaucheTreasures[i];
-            rowValues[k] = gaucheValues[i];
+            monsters[k] = leftMonsters[i];
+            treasures[k] = leftTreasures[i];
+            rowValues[k] = leftRowValues[i];
             i++;
             k++;
         }
 
-        // Ajouter les rangées restantes de droite
         while (j < n2) {
-            monsters[k] = droiteMonsters[j];
-            treasures[k] = droiteTreasures[j];
-            rowValues[k] = droiteValues[j];
+            monsters[k] = rightMonsters[j];
+            treasures[k] = rightTreasures[j];
+            rowValues[k] = rightRowValues[j];
             j++;
             k++;
         }
